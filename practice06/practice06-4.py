c86257a965c473b472ac131c32e9157527c7f1bd
hidecology <hidecology0302@gmail.com>
2023-10-13 22:57:50 +0900

    fix issues

--- practice06/practice06-4.py
+++ practice06/practice06-4.py
@@ -1,3 +1,3 @@
-d = {'apple':10, 'grape':20, 'orange':30}
+d = {'apple':10, 'grape':20, 'orange':30, 'lemon':5}
 # 上記マップdのサイズを出力してください
 # 期待する出力：4
--- practice09/practice09-3.py
+++ practice09/practice09-3.py
@@ -1,2 +1,3 @@
 question = 'How many characters?'
 # 上記の文字列の文字数を出力してください
+# 期待する出力：20
